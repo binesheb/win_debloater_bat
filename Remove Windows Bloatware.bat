d08a4767fbb544d10bc0406f2fd145a31edb6014
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-22 00:21:41 +0530

    fix: use explicit Appx package identifiers

--- Remove Windows Bloatware.bat
+++ Remove Windows Bloatware.bat
@@ -1,83 +1,84 @@
 @echo off
+setlocal EnableExtensions EnableDelayedExpansion
 
-echo Removing bloatware from Windows...
+echo Windows Debloater (Batch)
+echo.
 
-setlocal EnableDelayedExpansion
+rem Keep package identities explicit. Display names are used only for the menu.
+set "app_name[0]=3D Viewer"
+set "app_id[0]=Microsoft.Microsoft3DViewer"
+set "app_name[1]=Alarms and Clock"
+set "app_id[1]=Microsoft.WindowsAlarms"
+set "app_name[2]=Feedback Hub"
+set "app_id[2]=Microsoft.WindowsFeedbackHub"
+set "app_name[3]=Get Help"
+set "app_id[3]=Microsoft.GetHelp"
+set "app_name[4]=Groove Music"
+set "app_id[4]=Microsoft.ZuneMusic"
+set "app_name[5]=Microsoft Solitaire Collection"
+set "app_id[5]=Microsoft.MicrosoftSolitaireCollection"
+set "app_name[6]=Paint 3D"
+set "app_id[6]=Microsoft.MSPaint"
+set "app_name[7]=Skype"
+set "app_id[7]=Microsoft.SkypeApp"
+set "app_name[8]=Snipping Tool"
+set "app_id[8]=Microsoft.ScreenSketch"
+set "app_name[9]=Sticky Notes"
+set "app_id[9]=Microsoft.MicrosoftStickyNotes"
+set "app_name[10]=Tips"
+set "app_id[10]=Microsoft.Getstarted"
+set "app_name[11]=Phone Link"
+set "app_id[11]=Microsoft.YourPhone"
+set /a app_count=12
 
-rem Create an array of the applications to remove
-set "applications[0]=3D Viewer"
-set "applications[1]=Alarms & Clock"
-set "applications[2]=Feedback Hub"
-set "applications[3]=Get Help"
-set "applications[4]=Groove Music"
-set "applications[5]=Microsoft Solitaire Collection"
-set "applications[6]=News & Interests"
-set "applications[7]=Paint 3D"
-set "applications[8]=Skype"
-set "applications[9]=Snip & Sketch"
-set "applications[10]=Sticky Notes"
-set "applications[11]=Tips"
-set "applications[12]=Your Phone"
-
-rem Show a list of the applications and prompt the user to select which ones to remove
-set /a index=0
-set "removeApplications="
-set "keepApplications="
 :prompt
 cls
-echo Please select the applications to remove:
-for /L %%i in (0,1,12) do (
+echo Select applications to remove:
+echo Enter numbers separated by spaces, or press Enter to cancel.
+echo.
+for /L %%i in (0,1,11) do (
   set /a number=%%i+1
-  set "application=!applications[%%i]!"
-  echo !number!. !application!
+  echo !number!. !app_name[%%i]!
 )
-set /p "selection=Enter the numbers (separated by spaces) of the applications to remove, or press Enter to keep all: "
-if "%selection%"=="" goto keepAll
+set "selection="
+set /p "selection=Selection: "
+if not defined selection goto end
+
+set "selected_indexes="
 for %%i in (%selection%) do (
   set /a index=%%i-1
-  if not defined applications[!index!] (
-    echo Invalid selection: !selection!
-    pause
-    goto prompt
-  )
-  set "removeApplications=!removeApplications!,!applications[!index!]!"
+  if !index! LSS 0 goto invalid
+  if !index! GEQ %app_count% goto invalid
+  set "selected_indexes=!selected_indexes! !index!"
 )
-set "removeApplications=%removeApplications:~1%"
-goto confirm
 
-:keepAll
-set "keepApplications=!applications[0]!"
-for /L %%i in (1,1,12) do (
-  set "keepApplications=!keepApplications!,!applications[%%i]!"
-)
-goto confirm
-
-:confirm
 echo.
-if defined removeApplications (
-  echo The following applications will be removed:
-  echo %removeApplications%
-  echo.
-  set /p "confirm=Do you want to continue? (Y/N): "
-  if /i not "%confirm%"=="Y" goto end
-) else (
-  echo No applications will be removed.
-  pause
-  goto end
-)
+echo The following applications will be removed for the current user:
+for %%i in (!selected_indexes!) do echo - !app_name[%%i]!
+echo.
+choice /C YN /N /M "Continue"
+if errorlevel 2 goto end
 
-rem Remove the selected applications
-for /L %%i in (0,1,12) do (
-  set "application=!applications[%%i]!"
-  if "!removeApplications:,%application%,=!"=="%removeApplications%" (
-    set "keepApplications=!keepApplications!,!application!"
+for %%i in (!selected_indexes!) do (
+  echo Removing !app_name[%%i]!...
+  powershell -NoProfile -ExecutionPolicy Bypass -Command "$pkg=Get-AppxPackage -Name '!app_id[%%i]!'; if($pkg){$pkg ^| Remove-AppxPackage -ErrorAction Stop; exit 0}else{Write-Host 'Not installed for the current user.'; exit 0}"
+  if errorlevel 1 (
+    echo Failed to remove !app_name[%%i]!.
   ) else (
-    echo Removing !application!...
-    powershell -Command "Get-AppxPackage *!application:* = *! | Remove-AppxPackage"
+    echo Done.
   )
 )
 
-echo Bloatware removal complete.
+echo.
+echo Removal run complete. Review any failures above.
 pause
 
 :end
+endlocal
+exit /b 0
+
+:invalid
+echo.
+echo Invalid selection. Enter only numbers from 1 to %app_count%.
+pause
+goto prompt
